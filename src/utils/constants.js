972b61e843df888e14dbeed1c741387258996ae2
nth2612 <nguyentuanhaivjppro100@gmail.com>
2024-10-18 13:40:46 +0700

    Sua file constant

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,10 @@
-export const API_ROOT = 'http://localhost:8017'
+// export const API_ROOT = 'http://localhost:8017'
 
-// let apiRoot = ''
-// if (process.env.BUILD_MODE === 'dev') {
-//   apiRoot = 'http://localhost:8017'
-// }
-// if (process.env.BUILD_MODE === 'production') {
-//   apiRoot = 'https://trello-be-pafz.onrender.com'
-// }
-// export const API_ROOT = apiRoot
+let apiRoot = ''
+if (import.meta.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+if (import.meta.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-be-nnkb.onrender.com'
+}
+export const API_ROOT = apiRoot
