a175cf0f4ebb791dc23ef47c3391103b661775d3
nth2612 <nguyentuanhaivjppro100@gmail.com>
2024-10-18 14:01:12 +0700

    fix constant variables

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,10 @@
 // export const API_ROOT = 'http://localhost:8017'
 
 let apiRoot = ''
-if (import.meta.env.BUILD_MODE === 'dev') {
+if (import.meta.env.VITE_BUILD_MODE === 'dev') {
   apiRoot = 'http://localhost:8017'
 }
-if (import.meta.env.BUILD_MODE === 'production') {
+if (import.meta.env.VITE_BUILD_MODE === 'production') {
   apiRoot = 'https://trello-be-nnkb.onrender.com'
 }
 export const API_ROOT = apiRoot
