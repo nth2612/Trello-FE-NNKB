1486b44e7e1097cd504a2e066bda549178076072
nth2612 <nguyentuanhaivjppro100@gmail.com>
2024-10-18 13:46:19 +0700

    thay doi route

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <Routes>
       {/* Khi truy cap trang web thi tro ngay toi login */}
-      <Route path='/' element={<Navigate to='/board' replace={true} />} />
+      <Route path='/' element={<Navigate to='/login' replace={true} />} />
       <Route path='/login' element={<LoginPage/>} />
       <Route path='/signup' element={<SignUpPage/>} />
       <Route path='/user' element={<UserPage/>}/>
